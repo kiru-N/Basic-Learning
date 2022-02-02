86d8b1d71c38f454d6ecf0e4e46c50972b7321d0
kiru-N <kselvi28@gmail.com>
2022-02-02 10:10:48 +0800

    Code for simple puzle

---
+++ simple_puzzle.py
@@ -0,0 +1,24 @@
+# simple program
+# -------------
+
+print('Hi, I can code in Python!')
+print('\nMy favorite animal is Dog')
+print('''o-###-
+  | |   #''')
+
+print('\n This is my Home')
+
+print('''
+  __|__  
+ |     | 
+ |#    |______
+ |     |      |
+ |    #|    # |
+_|_____|__#___|__
+''')
+
+print('Now Puzzle time')
+birth_year = int(input('What year were you born? '))
+
+age = 2025 - birth_year
+print(f'In year 2025, you \'ll be {age} years old')
